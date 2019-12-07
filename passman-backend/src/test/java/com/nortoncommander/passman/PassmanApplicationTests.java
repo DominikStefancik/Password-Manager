328c53e330ef9c834f15b22f84b1205da2b88c4d
Dominik Stefancik <dstefancik@gmail.com>
2019-12-07 23:20:39 +0100

    Create skeleton of the passman backend

---
+++ passman-backend/src/test/java/com/nortoncommander/passman/PassmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nortoncommander.passman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassmanApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
